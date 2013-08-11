b86a416ad0ad7a92cb2626690628d888b2585e43
anshul <anshul.katare24@gmail.com>
2013-08-11 17:10:23 +0530

    my  commit
    this is my commit

--- src/com/marketing/cubicle/genericcart/activity/MainActivity.java
+++ src/com/marketing/cubicle/genericcart/activity/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	String newstring = "mystring";
 String gitcheck="ankit";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
